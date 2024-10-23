3b01f72097cf4695b2a36c98c52b8510317b4d15
leslieruan <leslieruan8@gmail.com>
2024-10-23 21:16:17 +0100

    Add auto search functionality with info display

--- src/SearchDropdown.js
+++ src/SearchDropdown.js
@@ -6,6 +6,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 export default function SearchDropdown() {
    const [musicData, setMusicData] = React.useState([]);
    const [options, setOptions] = React.useState([]);
+   const [selection, setSelection] = React.useState(null);
 
    React.useEffect(() =>{
     // fetch the local json data
@@ -26,15 +27,16 @@ export default function SearchDropdown() {
 
 
    const artNames = musicData.map((artist) => artist.name);
-// handle the input changes
+
 
 //    const test = (e) => {
 //     let text = e.target.value;
 //     let result = artNames.map();
 //     setOptions(result)
 //    }
+
+// function to handle the input changes
    const handleInput = (event, value) =>{
-    console.log(value);
     if(value){
         const result = artNames.filter((name) => 
             name.toLowerCase().includes(value.toLowerCase())
@@ -43,15 +45,48 @@ export default function SearchDropdown() {
     }else{
         setOptions([]); 
     }
-   };
+};
+
+ // function to find the select data
+ const handleSelection = (event, value) =>{
+    if(value){
+        const selectResult = musicData.find(e => e.name === value);
+        setSelection(selectResult);
+    }
+    else {
+        setSelection(null); 
+    }
+};
 
    return(
+    <div className='searchDropdown'>
     <Autocomplete
       disablePortal
       options={options}
       sx={{ width: 300 }}
       onInputChange={handleInput}
-      renderInput={(params) => <TextField {...params} label="Artist" />}
+      onChange={handleSelection}
+      renderInput={(params) => <TextField {...params} label="Search" />}
     />
+    {selection && (
+        <div className='music-info'>
+            <h4>Artist: {selection.name}</h4>
+            {selection.albums.map((album, index) => (
+                <div key={index} className="album-info">
+                    <h5>Album: {album.title}</h5>
+                    <p>{album.description}</p>
+                    <ul>
+                    {album.songs.map((song, idx) => (
+                                    <li key={idx}>
+                                        {song.title} - {song.length}
+                                    </li>
+                                ))}
+                    </ul>
+                </div>
+            ))
+            }
+        </div> 
+    )}
+    </div>
    );
 }
