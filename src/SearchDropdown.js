e37ee46cf7c41327a716277dabdece953ea84fc4
leslieruan <leslieruan8@gmail.com>
2024-10-24 21:57:40 +0100

    add the  navbar ,footer and grid layout

--- src/SearchDropdown.js
+++ src/SearchDropdown.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { Box, TextField, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Box, TextField, Select, MenuItem, FormControl, InputLabel, Card, CardContent, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
+import { Grid } from '@mui/system';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 
@@ -34,7 +36,7 @@ export default function SearchDropdown() {
                 console.error("Error fetching the data:", error);
             })
     }, []);
-    
+
 
     const handleSearchTypeChange = async (event) => {
         setSearchType(event.target.value);
@@ -51,21 +53,21 @@ export default function SearchDropdown() {
             setOptions([]);
             return;
         }
-        
+
         const searchTxt = value.toLowerCase();
         // only filter the empty strings 
         const searchWords = searchTxt.split(' ').filter(word => word !== ' ');
         let searchResults = [];
-      
+
         musicData.forEach(artist => {
-            if (searchType === 'name'){
-            const artistWords = artist.name.toLowerCase().split(' ');
-            const basicMacth = artist.name.toLowerCase().includes(searchWords);
-            const wordMatch = searchWords.length > 0 && searchWords.every(w => artist.name.toLowerCase().includes(w));
-            const unorderedMatch = searchWords.length > 0 && searchWords.every(searchw => 
-                artistWords.some(artistw => artistw.includes(searchw)))
-            // search artist 
-            if (basicMacth || wordMatch || unorderedMatch) {
+            if (searchType === 'name') {
+                const artistWords = artist.name.toLowerCase().split(' ');
+                const basicMacth = artist.name.toLowerCase().includes(searchWords);
+                const wordMatch = searchWords.length > 0 && searchWords.every(w => artist.name.toLowerCase().includes(w));
+                const unorderedMatch = searchWords.length > 0 && searchWords.every(searchw =>
+                    artistWords.some(artistw => artistw.includes(searchw)))
+                // search artist 
+                if (basicMacth || wordMatch || unorderedMatch) {
                     searchResults.push({
                         type: 'artist',
                         name: artist.name,
@@ -74,19 +76,19 @@ export default function SearchDropdown() {
                         wordMatch,
                         unorderedMatch
                     });
-                
+
+                }
             }
-        }
             // search albums
-            if (searchType === 'album' || searchType === 'song'){
+            if (searchType === 'album' || searchType === 'song') {
                 artist.albums.forEach(album => {
                     const albumtWords = album.title.toLowerCase().split(' ');
                     const albumbasicMacth = album.title.toLowerCase().includes(searchWords);
-                    const albumWordMatch = searchWords.length > 0 &&searchWords.every(w =>  album.title.toLowerCase().includes(w));
-                    const unorderedAlbumMatch = searchWords.every(searchw => 
-                        albumtWords.some(albumw => albumw.includes(searchw))  )
+                    const albumWordMatch = searchWords.length > 0 && searchWords.every(w => album.title.toLowerCase().includes(w));
+                    const unorderedAlbumMatch = searchWords.every(searchw =>
+                        albumtWords.some(albumw => albumw.includes(searchw)))
 
-                    if (albumbasicMacth || albumWordMatch ||unorderedAlbumMatch) {
+                    if (albumbasicMacth || albumWordMatch || unorderedAlbumMatch) {
                         searchResults.push({
                             type: 'album',
                             artistName: artist.name,
@@ -98,14 +100,14 @@ export default function SearchDropdown() {
                         });
                     }
                     // search song
-                    if(searchType === 'song'){
+                    if (searchType === 'song') {
                         album.songs.forEach(song => {
                             const songWords = song.title.toLowerCase().split(' ');
                             const songbasicMacth = song.title.toLowerCase().includes(searchWords);
-                            const songWordMatch = searchWords.every(w =>  album.title.toLowerCase().includes(w));
-                            const unorderedSongMatch = searchWords.every(searchw => 
-                                songWords.some(songw => songw.includes(searchw))  )
-                            if (songbasicMacth || songWordMatch ||unorderedSongMatch) {
+                            const songWordMatch = searchWords.every(w => album.title.toLowerCase().includes(w));
+                            const unorderedSongMatch = searchWords.every(searchw =>
+                                songWords.some(songw => songw.includes(searchw)))
+                            if (songbasicMacth || songWordMatch || unorderedSongMatch) {
                                 searchResults.push({
                                     type: 'song',
                                     name: song.title,
@@ -119,14 +121,14 @@ export default function SearchDropdown() {
                             }
                         });
                     }
-                    }
+                }
                 );
             }
         });
-        
-         // sort the search result  basic > word >unorder
-        searchResults.sort((a, b) =>{
-            if(a.basicMacth !== b.basicMacth){
+
+        // sort the search result  basic > word >unorder
+        searchResults.sort((a, b) => {
+            if (a.basicMacth !== b.basicMacth) {
                 return a.basicMatch ? -1 : 1;
             }
             if (a.wordMatch !== b.wordMatch) {
@@ -136,9 +138,9 @@ export default function SearchDropdown() {
                 return a.unorderedMatch ? -1 : 1;
             }
             return 0;
-        })      
+        })
         searchResults = searchResults.slice(0, 10);
-        setOptions(searchResults);       
+        setOptions(searchResults);
     };
 
     // function to find the select data
@@ -181,82 +183,117 @@ export default function SearchDropdown() {
 
     return (
         <div className='searchDropdown'>
-            <Box>
-                <FormControl>
-                    <InputLabel id="search-type-label">Search Type</InputLabel>
-                    <Select
-                        labelId="search-type-label"
-                        value={searchType}
-                        label="search type"
-                        onChange={handleSearchTypeChange}>
-                        {searchTypes.map((type) => (
-                            <MenuItem key={type.value} value={type.value}>
-                                {type.label}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-            </Box>
-            <Autocomplete
-                key={searchType} 
-                disablePortal
-                options={options}
-                sx={{ width: 300 }}
-                freeSolo ={true}
-                onInputChange={(event, newValue) => {
-                    handleInput(event, newValue);
-                }}
-                onChange={handleSelection}
-                inputValue={inputValue}
-                getOptionLabel={(option) => option.display || ''}
-                filterOptions={(options) => options} 
-                renderInput={(params) => <TextField {...params} label="Search" />}
-            />
-            {selection && (
-                <div className='music-info'>
-                    {selection.type === 'artist' && (
-                        <div>
-                            <h4>Artist:{selection.data.name}</h4>
-                            {selection.data.albums.map((album, index) => (
-                                <div key={index} className="album-info">
-                                    <h5>album:{album.title}</h5>
-                                    <p>{album.description}</p>
-                                    <ul>
-                                        {album.songs.map((song, idx) => (
-                                            <li key={idx}>
-                                                {song.title} - {song.length}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
+            <Grid className="search-container" container>
+                <Box item>
+                    <FormControl>
+                        <InputLabel id="search-type-label">Type</InputLabel>
+                        <Select
+                            labelId="search-type-label"
+                            value={searchType}
+                            label="search type"
+                            onChange={handleSearchTypeChange}>
+                            {searchTypes.map((type) => (
+                                <MenuItem key={type.value} value={type.value}>
+                                    {type.label}
+                                </MenuItem>
                             ))}
-                        </div>
-                    )}
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Grid item>
+                    <Autocomplete
+                        key={searchType}
+                        disablePortal
+                        options={options}
+                        sx={{ width: 300 }}
+                        // freeSolo={true}
+                        onInputChange={(event, newValue) => {
+                            handleInput(event, newValue);
+                        }}
+                        onChange={handleSelection}
+                        inputValue={inputValue}
+                        getOptionLabel={(option) => option.display || ''}
+                        filterOptions={(options) => options}
+                        renderInput={(params) => <TextField {...params} label="Search" />}
+                    />
+                </Grid>
 
-                    {selection.type === 'album' && (
-                        <div>
-                            <h4>album: {selection.data.title}</h4>
-                            <h5>artist: {selection.artistName}</h5>
-                            <p>{selection.data.description}</p>
-                            <ul>
-                                {selection.data.songs.map((song, idx) => (
-                                    <li key={idx}>
-                                        {song.title} - {song.length}
-                                    </li>
+            </Grid>
+            <Grid className="info-container" container spacing={2}>
+                {selection && (
+                    <div className='music-info'>
+                        {selection.type === 'artist' && (
+                            <Grid item xs={12}>
+                                <Card className="artist-card">
+                                    <CardContent>
+                                        <Typography variant="h5">{selection.data.name}</Typography>
+                                    </CardContent>
+                                </Card>
+                                {selection.data.albums.map((album, index) => (
+                                    <Accordion key={index} className="album-accordion">
+                                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`album-content-${index}`} id={`album-header-${index}`}>
+                                            <Typography variant="h6">{album.title}</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <Typography>{album.description}</Typography>
+                                            <ul>
+                                                {album.songs.map((song, idx) => (
+                                                    <li key={idx}>
+                                                        <Typography variant="body2">
+                                                            {song.title} - {song.length}
+                                                        </Typography>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </AccordionDetails>
+                                    </Accordion>
                                 ))}
-                            </ul>
-                        </div>
-                    )}
-                    {selection.type === 'song' && (
-                        <div>
-                            <h4>Song: {selection.data.title}</h4>
-                            <h4>Album: {selection.albumName}</h4>
-                            <h5>Artist: {selection.artistName}</h5>
-                            <p>{selection.data.length}</p>
-                        </div>
-                    )}
-                </div>
-            )}
+
+                            </Grid>
+                        )}
+                        {selection.type === 'album' && (
+                           <Grid item xs={12}>
+                           <Card className="album-card">
+                             <CardContent>
+                               <Typography variant="h5">{selection.data.title}</Typography>
+                               <Typography variant="h6">Artist: {selection.artistName}</Typography>
+                               <Typography >{selection.data.description}</Typography>
+                             </CardContent>
+                           </Card>
+                           <Accordion className="songs-accordion">
+                             <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="songs-content" id="songs-header">
+                               <Typography variant="h6">Songs</Typography>
+                             </AccordionSummary>
+                             <AccordionDetails>
+                               <ul>
+                                 {selection.data.songs.map((song, idx) => (
+                                   <li key={idx}>
+                                     <Typography variant="body2">
+                                       {song.title} - {song.length}
+                                     </Typography>
+                                   </li>
+                                 ))}
+                               </ul>
+                             </AccordionDetails>
+                           </Accordion>
+                         </Grid>
+                        )}
+                        {selection.type === 'song' && (
+                            <Grid item xs={12}>
+                            <Card className="song-card">
+                              <CardContent>
+                                <Typography variant="h5"> {selection.data.title}</Typography>
+                                <Typography variant="h6">Album: {selection.albumName}</Typography>
+                                <Typography variant="p">Artist: {selection.artistName}</Typography>
+                                <Typography>Length: {selection.data.length}</Typography>
+                              </CardContent>
+                            </Card>
+                          </Grid>
+                        )}
+                    </div>
+                )}
+            </Grid>
+
         </div>
     );
 }
