08b24542ae4c3f41b27f282f3a850e8ebe1754c2
leslieruan <leslieruan8@gmail.com>
2024-10-28 15:17:42 +0000

    remove the nav bar and footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import './App.css';
 import './SearchDropdown';
 import SearchDropdown from './SearchDropdown';
-import NavBar from './NavBar';
-import Footer from './Footer'
+// import NavBar from './NavBar';
+// import Footer from './Footer'
 function App() {
   return (
     <div className="App">
       {/* <NavBar></NavBar> */}
       <main className='main-container'>
        <SearchDropdown></SearchDropdown>
+     
       </main>
     {/* <Footer></Footer> */}
     </div>
